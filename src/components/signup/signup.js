85d57f824b55902125d4dc66e6b96dcca967ee0d
goz4el <hordemzerado@gmail.com>
2021-10-17 11:07:29 -0400

    Building the signup component

---
+++ src/components/signup/signup.js
@@ -0,0 +1,26 @@
+import "./signup.css";
+import React from "react";
+import Card from "@material-ui/core/card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import { Component } from "react";
+
+class SignUp extends Component {
+  render() {
+    return (
+      <div className="card">
+        <Card>
+          <CardContent>
+            <h1>Registration Form Here</h1>
+          </CardContent>
+          <CardActions>
+            <Button> Button </Button>
+          </CardActions>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
