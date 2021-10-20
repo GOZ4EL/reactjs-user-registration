a2270b446fdf1380a87a6410c63385fbdf1b2c00
goz4el <hordemzerado@gmail.com>
2021-10-20 13:57:40 -0400

    Starting to build the signup form

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -4,18 +4,23 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
+import TextBox from "../../core/textField/textField";
 import { Component } from "react";
 
 class SignUp extends Component {
   render() {
     return (
       <div className="card">
-        <Card>
+        <Card className="cardStyle">
           <CardContent>
-            <h1>Registration Form Here</h1>
+            <div className="signupText">SIGNUP</div>
+            <TextBox label="Full Name" />
           </CardContent>
-          <CardActions>
-            <Button> Button </Button>
+
+          <CardActions className="CardActions">
+            <Button style={{ background: "black", color: "white" }}>
+              SIGNUP
+            </Button>
           </CardActions>
         </Card>
       </div>
